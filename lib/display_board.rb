fc3448957c6ff7d70826e9b010667fa608ba7ac3
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-23 15:22:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 
   def display_board(board)
-    board = [" "," "," "," "," "," "," "," "," "]
+    
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
